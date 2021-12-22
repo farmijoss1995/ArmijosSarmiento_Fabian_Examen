4436c63eda584e86925cf70ef8ebe3d6157067a6
farmijoss1995 <marceloas@hotmail.es>
2021-12-22 08:48:03 -0500

    9Commit

--- ArmijosSarmiento_Fabian_Examen/src/main/java/ec/ups/edu/ON/GestionAutor.java
+++ ArmijosSarmiento_Fabian_Examen/src/main/java/ec/ups/edu/ON/GestionAutor.java
@@ -1,5 +1,41 @@
 package ec.ups.edu.ON;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import ec.ups.edu.DAO.AutorDAO;
+import ec.ups.edu.DAO.CapituloDAO;
+import ec.ups.edu.DAO.LibroDAO;
+import ec.ups.edu.EN.Autor;
+import ec.ups.edu.EN.Capitulo;
+import ec.ups.edu.EN.Libro;
+
+@Stateless
 public class GestionAutor {
 
+	@Inject
+	private LibroDAO ldao;
+	@Inject
+	private CapituloDAO cdao;
+	
+	@Inject
+	private AutorDAO adao;
+	
+	public void guardarLibro(Libro libro) {
+		ldao.insertar(libro);
+	}
+	
+	public List<Libro> listadoLibros(){
+		return ldao.listar();
+	}
+	
+	public List<Capitulo> libCapitulos(int idLibro){
+		return cdao.listar(idLibro);
+	}
+	
+	public Autor busqueda(int idAutor) {
+		return adao.buscar(idAutor);
+	}
 }
